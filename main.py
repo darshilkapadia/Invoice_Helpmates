c40d5522b1569e55e8cc5309aa0331aa9b7ceaf5
darshilkapadia <97408199+darshilkapadia@users.noreply.github.com>
2022-12-15 19:05:45 -0500

    Updated

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-import os
+from flask.views import MethodView
+from wtforms import Form
+
+class Homepage(MethodView):
+    pass
+
+
+class BillFormPage(MethodView):
+    pass
+
+
+class ResultsPage(MethodView):
+    pass
+
+
+class BillForm(Form):
+    pass
+
+
